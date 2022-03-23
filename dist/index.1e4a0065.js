245f736737fdf16e5b7cc26373fec2f18e9fda7f
prplblck <kariurek@gmail.com>
2022-03-23 21:50:18 +0100

    style base

---
+++ dist/index.1e4a0065.js
@@ -0,0 +1,5 @@
+console.log('hello parcel');
+const test = 123;
+console.log(test);
+
+//# sourceMappingURL=index.1e4a0065.js.map
---
+++ src/assets/scripts/app.js
@@ -0,0 +1,4 @@
+console.log('hello parcel');
+const test = 123;
+console.log(test);
+console.log('test code new');

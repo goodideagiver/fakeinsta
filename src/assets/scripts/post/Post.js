a36390889bdbe1b2b98c00515a80f41078d975b2
prplblck <kariurek@gmail.com>
2022-03-26 13:50:05 +0100

    Updates

--- src/assets/scripts/post/Post.js
+++
@@ -1,8 +0,0 @@
-import { getRandomName } from './data/Names.js';
-import { getRandomSentence } from './data/Desc.js';
-import { PostUtils } from './PostUtils.js';
-export class Post extends PostUtils {
-	constructor(username, description, image) {
-		super();
-	}
-}

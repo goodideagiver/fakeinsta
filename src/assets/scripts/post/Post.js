dc7c59f13cb39f1e3e0f2dcac22b4272a8ce0707
Karol <kariurek@gmail.com>
2022-03-31 09:36:24 +0200

    remove unsed imports

--- src/assets/scripts/post/Post.js
+++ src/assets/scripts/post/Post.js
@@ -1,5 +1,3 @@
-import { getRandomName } from './data/Names.js';
-import { getRandomSentence } from './data/Desc.js';
 import { PostUtils } from './PostUtils.js';
 export class Post extends PostUtils {
 	constructor(username, description, image) {

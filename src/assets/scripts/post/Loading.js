c98c7a0f4a2e251f7cf528d8f88e88b043d56449
Karol <kariurek@gmail.com>
2022-04-05 07:48:49 +0200

    added loading

--- src/assets/scripts/post/Loading.js
+++ src/assets/scripts/post/Loading.js
@@ -12,17 +12,35 @@ class LoadingElement extends HTMLElement {
                         transform: rotate(360deg);
                     }
                 }
+                @keyframes fading {
+                    0% {
+                        background-color: rgba(0, 0, 0, 0);
+                    }
+                    50% {
+                        background-color: rgba(0, 0, 0, 0.1);
+                    }
+                    100% {
+                        background-color: rgba(0, 0, 0, 0);
+                    }
+                }
+
+                
                 .loading-container {
                     height: 100%;
                     margin: 20px 0;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    animation: fading 1.0s linear infinite;
                 }
                 .loading-icon {
-                    animation: spin 1s linear infinite;
+                    animation: spin 1.5s linear infinite;
                     height: 100px;
                     width: 100px;
                     border-radius: 50%;
-                    border: 2px solid #fff;
+                    border: 2px solid transparent;
                     border-top: 2px solid #000;
+                    border-bottom: 2px solid #000;
                     display: flex;
                     justify-content: center;
                     align-items: center;

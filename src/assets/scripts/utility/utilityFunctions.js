0740811e5691cbcc74f66da2a729b67a568ade01
prplblck <kariurek@gmail.com>
2022-04-06 21:54:49 +0200

    refactor to async

--- src/assets/scripts/utility/utilityFunctions.js
+++ src/assets/scripts/utility/utilityFunctions.js
@@ -2,11 +2,11 @@
 export const getRandomInt = (min, max) =>
 	Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const unsupportedFeatureAlert = () => {
-	import('../components/Modal').then(() => {
-		const modal = document.createElement('fakeinsta-modal');
-		modal.textContent =
-			'This function is unsupported and probably will never be added.';
-		document.getElementById('app').prepend(modal);
-	});
+export const unsupportedFeatureAlert = async () => {
+	await import('../components/Modal');
+
+	const modal = document.createElement('fakeinsta-modal');
+	modal.textContent = 'This function is unsupported and probably will never be added.';
+
+	document.getElementById('app').prepend(modal);
 };
